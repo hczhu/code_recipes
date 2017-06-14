4b6b41f4031da8b86f896951dbb4270e7385f11b
hczhu <zhuhcheng@gmail.com>
2017-06-13 23:15:03 -0700

    add files

---
+++ atomic-mem-order.cpp
@@ -0,0 +1,43 @@
+#include "header.h"
+
+void relaxedMagic1() {
+  std::atomic<int> x(0), y(0);
+  std::atomic<bool> loaded{false};
+  bool stop = false;
+  std::mutex mt;
+  std::condition_variable cv;
+  std::thread writer([&]{
+    while (!stop) {
+      x.store(1,std::memory_order_relaxed);
+      y.store(1,std::memory_order_relaxed);
+      std::unique_lock<std::mutex> lk(mt);
+      cv.wait(lk, [&]{
+        return stop || loaded;
+      });
+      loaded = false;
+    }
+  });
+
+  std::thread reader([&]{
+    int round = 0;
+    while (!stop) {
+      int a = y.load(std::memory_order_relaxed);
+      int b = x.load(std::memory_order_relaxed);
+      std::cout << "Round #" << ++round << std::endl;
+      if (a==1 && b!=1) {
+        std::cout << "Magic happend: a == 1 && b != 1." << std::endl;
+        stop = true;
+      }
+      loaded = true;
+      cv.notify_one();
+    }
+  });
+
+  reader.join();
+  writer.join();
+}
+
+int main() {
+  relaxedMagic1();
+  return 0;
+}
---
+++ header.h
@@ -0,0 +1,30 @@
+#include <algorithm>
+#include <array>
+#include <atomic>
+#include <cassert>
+#include <cmath>
+#include <complex>
+#include <condition_variable>
+#include <cstdarg>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <functional>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <limits.h>
+#include <map>
+#include <memory>
+#include <mutex>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <string>
+#include <thread>
+#include <valarray>
+#include <vector>
+
+//using namespace std;
+#define debug(x) std::cerr<<#x<<"=\""<<x<<"\""<<" at line#"<<__LINE__<< std::endl;
