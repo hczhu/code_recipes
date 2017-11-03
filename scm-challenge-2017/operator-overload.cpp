308911acd1281c3365a83a462dfcc41c8ec39f81
hczhu <zhuhcheng@gmail.com>
2017-11-02 22:40:33 -0700

    Add files

---
+++ scm-challenge-2017/operator-overload.cpp
@@ -0,0 +1,104 @@
+
+#include <algorithm>
+#include <array>
+#include <atomic>
+#include <cassert>
+#include <cmath>
+#include <complex>
+#include <condition_variable>
+#include <cstdarg>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <functional>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <limits.h>
+#include <map>
+#include <memory>
+#include <mutex>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <string>
+#include <thread>
+#include <valarray>
+#include <vector>
+
+#include <glog/logging.h>
+#include <gflags/gflags.h>
+#include <gtest/gtest.h>
+
+template<typename T>
+class _DisplayType;
+
+template<typename T>
+void _displayType(T&& t);
+
+#define PEEK(x) LOG(INFO) << #x << ": [" << (x) << "]"
+
+/* template end */
+
+#include <cassert>
+
+int comp(const char *a, const char *b) {
+  while (*a && *b) {
+    auto la = tolower(*a++);
+    auto lb = tolower(*b++);
+    if (la != lb) {
+      return (int)la - lb;
+    }
+  }
+  return int(*a) - *b;
+}
+
+class CIString {
+ public:
+	CIString(std::string _s): s_(_s) {};
+	CIString(const char* s): p_(s) {};
+  const char* c_str() const {
+    return p_ ? p_ : s_.c_str();
+  }
+
+ private:
+	std::string s_;
+  const char* p_ = nullptr;
+};
+
+bool operator<(const CIString &cs1, const CIString &cs) {
+  return comp(cs1.c_str(), cs.c_str()) < 0;
+}
+bool operator>(const CIString &cs1, const CIString &cs) {
+  return comp(cs1.c_str(), cs.c_str()) > 0;
+}
+bool operator<=(const CIString &cs1, const CIString &cs) {
+  return comp(cs1.c_str(), cs.c_str()) <= 0;
+}
+bool operator>=(const CIString &cs1, const CIString &cs) {
+  return comp(cs1.c_str(), cs.c_str()) >= 0;
+}
+bool operator==(const CIString &cs1, const CIString &cs) {
+  return comp(cs1.c_str(), cs.c_str()) == 0;
+}
+bool operator!=(const CIString &cs1, const CIString &cs) {
+  return comp(cs1.c_str(), cs.c_str()) != 0;
+}
+
+
+TEST(Foo, Bar) {
+  CIString a("ab");
+  CIString b("AB");
+  EXPECT_TRUE(a == b);
+  EXPECT_TRUE(a == "Ab");
+  EXPECT_TRUE("aB" == a);
+}
+
+int main(int argc, char* argv[]) {
+  testing::InitGoogleTest(&argc, argv);
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+  google::InitGoogleLogging(argv[0]);
+  return RUN_ALL_TESTS();
+}
+
