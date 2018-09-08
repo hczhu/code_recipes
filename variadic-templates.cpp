2f524f3e3098b762997b31e881b8f0c535d3c560
Kezun Sima <simakezun@outlook.com>
2018-09-07 20:04:30 -0700

    update

--- variadic-templates.cpp
+++ variadic-templates.cpp
@@ -40,13 +40,40 @@ void confirm(const Args&... msg) {
 
 }
 
+struct Vec {
+  template<typename... Args>
+  static std::shared_ptr<Vec> make(Args&&... eles) {
+    auto res = std::make_shared<Vec>();
+    res->add(std::forward<Args>(eles)...);
+    return res;
+  }
+  template<typename Arg1, typename... Args>
+  void add(Arg1 ele, Args&&... eles) {
+    add(ele);
+    add(eles...);
+  }
+  
+  template<typename Arg>
+  void add(Arg&& ele) {
+    vec_.push_back(int(ele));
+  }
 
+  std::vector<int> vec_;
+};
 
 int main() {
   print(cout, 1, 2, "sfsfd", 1.34243, 'a', 3241234);
   string ss;
   stringstream os(ss);
   print(os, 1, 2, "sfsfd", 1.34243, 'a', 3241234);
-  cout << os.str();
+  cout << os.str() << std::endl;
+
+  auto vec = Vec::make(1, 2, 4, 5);
+  print(cout,
+    vec->vec_[0],
+    vec->vec_[1],
+    vec->vec_[2],
+    vec->vec_[3]
+  );
   return 0;
 }
