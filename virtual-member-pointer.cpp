fe0b93a2a8ea3cc590c7c7bddb6676677f47044c
Kezun Sima <simakezun@outlook.com>
2018-10-23 13:59:26 -0700

    add a new file

---
+++ virtual-member-pointer.cpp
@@ -0,0 +1,75 @@
+
+#include <algorithm>
+#include <array>
+#include <atomic>
+#include <cassert>
+#include <cmath>
+#include <complex>
+#include <condition_variable>
+#include <cstdarg>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <fstream>
+#include <functional>
+#include <iostream>
+#include <iterator>
+#include <limits.h>
+#include <limits>
+#include <map>
+#include <memory>
+#include <mutex>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <string>
+#include <thread>
+#include <valarray>
+#include <vector>
+
+#include <gflags/gflags.h>
+#include <glog/logging.h>
+#include <gtest/gtest.h>
+
+template<typename T>
+class _DisplayType;
+
+template<typename T>
+void _displayType(T&& t);
+
+#define PEEK(x) LOG(INFO) << #x << ": [" << (x) << "]"
+
+/* template end */
+
+class A {
+ public:
+  virtual ~A() = default;
+  virtual int foo() const {
+    return 0;
+  }
+};
+
+class B : public A {
+ public:
+  int foo() const override {
+    return 1;
+  }
+};
+
+TEST(Foo, Bar) {
+  const B b;
+  const A* a = &b;
+  using Mptr = int(A::*)() const;
+  Mptr ptr = &A::foo;
+  LOG(INFO) << "virtual method pointer: " << ptr;
+  EXPECT_EQ(1, (a->*ptr)());
+}
+
+int main(int argc, char* argv[]) {
+  testing::InitGoogleTest(&argc, argv);
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+  google::InitGoogleLogging(argv[0]);
+  return RUN_ALL_TESTS();
+}
+
