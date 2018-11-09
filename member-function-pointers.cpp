4da712e25c7b3e287da503da35e1fcd7549e5507
Kezun Sima <simakezun@outlook.com>
2018-11-09 15:08:13 -0800

    update

---
+++ member-function-pointers.cpp
@@ -0,0 +1,76 @@
+
+#include <algorithm>
+#include <array>
+#include <atomic>
+#include <cassert>
+#include <cmath>
+#include <complex>
+#include <condition_variable>
+#include <cstdarg>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <fstream>
+#include <functional>
+#include <iostream>
+#include <iterator>
+#include <limits.h>
+#include <limits>
+#include <map>
+#include <memory>
+#include <mutex>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <string>
+#include <thread>
+#include <valarray>
+#include <vector>
+
+#include <gflags/gflags.h>
+#include <glog/logging.h>
+#include <gtest/gtest.h>
+// #include <gtest/gmock.h>
+
+template<typename T>
+class _DisplayType;
+
+template<typename T>
+void _displayType(T&& t);
+
+#define PEEK(x) LOG(INFO) << #x << ": [" << (x) << "]"
+
+struct B {
+ virtual ~B() = default;
+
+ virtual void foo() {}
+
+ void bar() { }
+
+ void den() { }
+};
+
+struct D : public B {
+
+ void foo() override {}
+
+ void bar() { }
+
+};
+
+/* template end */
+
+TEST(MemberPointerTest, Basic) {
+  EXPECT_EQ(&B::foo, &D::foo);
+  EXPECT_NE(&B::bar, &D::bar);
+  EXPECT_EQ(&B::den, &D::den);
+}
+
+int main(int argc, char* argv[]) {
+  testing::InitGoogleTest(&argc, argv);
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+  google::InitGoogleLogging(argv[0]);
+  return RUN_ALL_TESTS();
+}
+
