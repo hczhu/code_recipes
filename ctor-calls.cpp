30a6fa9edfca0f407799bb901bebdb32ce2d52c0
Kezun Sima <simakezun@outlook.com>
2018-11-14 21:43:54 -0800

    add a file

---
+++ ctor-calls.cpp
@@ -0,0 +1,57 @@
+
+#include <algorithm>
+#include <array>
+#include <atomic>
+#include <cassert>
+#include <cmath>
+#include <complex>
+#include <condition_variable>
+#include <cstdarg>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <fstream>
+#include <functional>
+#include <iostream>
+#include <iterator>
+#include <limits.h>
+#include <limits>
+#include <map>
+#include <memory>
+#include <mutex>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <string>
+#include <thread>
+#include <valarray>
+#include <vector>
+
+#include <gflags/gflags.h>
+#include <glog/logging.h>
+#include <gtest/gtest.h>
+#include "dummy-classes.h"
+
+template<typename T>
+class _DisplayType;
+
+template<typename T>
+void _displayType(T&& t);
+
+/* template end */
+
+TEST(Foo, Bar) {
+  auto bb = Base();
+  EXPECT_EQ(1, Base::CtorCnt);
+  EXPECT_EQ(0, Base::MoveCnt);
+  EXPECT_EQ(0, Base::CopyCnt);
+}
+
+int main(int argc, char* argv[]) {
+  testing::InitGoogleTest(&argc, argv);
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+  google::InitGoogleLogging(argv[0]);
+  return RUN_ALL_TESTS();
+}
+
