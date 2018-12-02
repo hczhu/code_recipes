688d7416c3000ab9740fa1343a29f6db47ea37c3
Kezun Sima <simakezun@outlook.com>
2018-12-01 22:35:58 -0800

    update

--- test-fstream.cpp
+++ test-fstream.cpp
@@ -9,17 +9,20 @@ void _displayType(T&& t);
 
 TEST(Foo, Bar) {
   using namespace std;
-  {
-    ofstream("/tmp/example.txt");
-  }
-  fstream myfile;
-  myfile.open("/tmp/example.txt", ios::in | ios::out);
+  fstream myfile("/tmp/example.txt",
+                 fstream::in | fstream::out | fstream::trunc);
   myfile << "a b c d";
   myfile.flush();
   std::string token;
   myfile.seekp(0);
   myfile >> token;
   EXPECT_EQ("a", token);
+  myfile >> token;
+  EXPECT_EQ("b", token);
+  myfile >> token;
+  EXPECT_EQ("c", token);
+  myfile >> token;
+  EXPECT_EQ("d", token);
 }
 
 int main(int argc, char* argv[]) {
