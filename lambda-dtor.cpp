6d20388d28ba49810d29f8b3ac52ed6ed6cc53fc
Kezun Sima <simakezun@outlook.com>
2018-12-14 23:38:01 -0800

    add a file

---
+++ lambda-dtor.cpp
@@ -0,0 +1,53 @@
+
+#include <algorithm>
+#include <array>
+#include <atomic>
+#include <cassert>
+#include <cmath>
+#include <complex>
+#include <condition_variable>
+#include <cstdarg>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <fstream>
+#include <functional>
+#include <iostream>
+#include <iterator>
+#include <limits.h>
+#include <limits>
+#include <map>
+#include <memory>
+#include <mutex>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <string>
+#include <thread>
+#include <valarray>
+#include <vector>
+
+#include <gflags/gflags.h>
+#include <glog/logging.h>
+#include <gtest/gtest.h>
+#include "dummy-classes.h"
+
+void bar(std::function<void()>&& fn) {
+
+}
+
+TEST(Foo, Bar) {
+  std::mutex mx;
+  std::unique_lock<std::mutex> lg(mx);
+  { auto lm = [_lg = move(lg)]{}; }
+  EXPECT_TRUE(mx.try_lock());
+}
+
+int main(int argc, char* argv[]) {
+  testing::InitGoogleTest(&argc, argv);
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+  google::InitGoogleLogging(argv[0]);
+  return RUN_ALL_TESTS();
+}
+
