67986f2c2f7d3a22b37652bab31d3096eca46d46
Kezun Sima <simakezun@outlook.com>
2019-02-22 10:55:06 -0800

    update

---
+++ reference-to-function.cpp
@@ -0,0 +1,67 @@
+
+#include <algorithm>
+#include <array>
+#include <atomic>
+#include <cassert>
+#include <cmath>
+#include <complex>
+#include <condition_variable>
+#include <cstdarg>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <fstream>
+#include <functional>
+#include <iostream>
+#include <iterator>
+#include <limits.h>
+#include <limits>
+#include <map>
+#include <memory>
+#include <mutex>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <string>
+#include <thread>
+#include <valarray>
+#include <vector>
+
+#include <gflags/gflags.h>
+#include <glog/logging.h>
+#include <gtest/gtest.h>
+// #include <gtest/gmock.h>
+
+template<typename T>
+class _DisplayType;
+
+template<typename T>
+void _displayType(T&& t);
+
+#define PEEK(x) LOG(INFO) << #x << ": [" << (x) << "]"
+
+/* template end */
+
+using func_ref_t = int(&)();
+
+int forward(func_ref_t f) {
+  return f();
+}
+
+int foo() {
+  return -1;
+}
+
+TEST(Foo, Bar) {
+  EXPECT_EQ(-1, forward(foo));
+  // EXPECT_EQ(1, forward([] { return 1; }));
+}
+
+int main(int argc, char* argv[]) {
+  testing::InitGoogleTest(&argc, argv);
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+  google::InitGoogleLogging(argv[0]);
+  return RUN_ALL_TESTS();
+}
+
