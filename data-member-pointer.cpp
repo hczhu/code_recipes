6c59367db9d98f92dab3691cce1ff4389c470cf6
HC Zhu <hczhu.mtv@gmail.com>
2020-03-02 13:48:00 -0800

    update

--- data-member-pointer.cpp
+++ data-member-pointer.cpp
@@ -50,11 +50,23 @@ struct Data {
   Inner inner;
 };
 
+template<class T, typename F>
+auto get_lambda(F T::*field) {
+  return [=](const T& t) -> F{
+    return t.*field;
+  };
+}
+
 int main(int argc, char* argv[]) {
   auto ptr = &Data::Inner::c;
   Data d;
   d.inner.c = true;
+  d.b = -1;
   PEEK(d.inner.*ptr);
+
+  auto lm = get_lambda(&Data::b);
+
+  EXPECT_EQ(-1, lm(d));
 
   // _DisplayType<decltype(&Data::Inner::c)> _a;
   return 0;
