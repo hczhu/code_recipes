f094214fa056d169d25cb2e02932e498ee6240cf
HC Zhu <hczhu.mtv@gmail.com>
2020-05-17 15:40:22 -0700

    update

---
+++ cpp/declare-mess.cpp
@@ -0,0 +1,68 @@
+
+#include <algorithm>
+#include <array>
+#include <atomic>
+#include <cassert>
+#include <cmath>
+#include <complex>
+#include <condition_variable>
+#include <cstdarg>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <fstream>
+#include <functional>
+#include <iostream>
+#include <iterator>
+#include <limits.h>
+#include <limits>
+#include <map>
+#include <memory>
+#include <mutex>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <string>
+#include <thread>
+#include <valarray>
+#include <vector>
+
+#include <gflags/gflags.h>
+#include <glog/logging.h>
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+template<typename T>
+class _DisplayType;
+
+template<typename T>
+void _displayType(T&& t);
+
+#define PEEK(x) LOG(INFO) << #x << ": [" << (x) << "]"
+
+#define _STR(x) #x
+#define STR(x) _STR(x)
+#define PRINT_MACRO(M) static_assert(0, "Macro " #M " = " STR(M))
+
+// #define x 42
+// PRINT_MACRO(x);
+
+/* template end */
+
+TEST(DeclareMess, Mess) {
+  float a = 99.9;
+  std::vector<int> b(size_t(a));
+  static_assert(std::is_same_v<decltype(b), std::vector<int>>);
+
+  std::vector<int> c{size_t(a)};
+  static_assert(std::is_same_v<decltype(c), std::vector<int>>);
+}
+
+int main(int argc, char* argv[]) {
+  testing::InitGoogleTest(&argc, argv);
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+  google::InitGoogleLogging(argv[0]);
+  return RUN_ALL_TESTS();
+}
+
