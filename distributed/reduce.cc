39e8adb3ba94a65222543317672676971eec1549
HC Zhu <hczhu.mtv@gmail.com>
2022-07-10 23:02:22 -0700

    update

--- distributed/reduce.cc
+++ distributed/reduce.cc
@@ -123,7 +123,6 @@ struct Message {
 
 void binaryTreeTreadRunWithMsg(size_t threadId, size_t n, int &value,
                                MessageQueue<Message> &mq) {
-  //LOG(INFO) << "Running thread #" << threadId;
   for (size_t r = 1; r < n; r <<= 1) {
     const auto rr = r << 1;
     if (threadId % rr) {
@@ -132,15 +131,10 @@ void binaryTreeTreadRunWithMsg(size_t threadId, size_t n, int &value,
                                   .v = value,
                                   .from = threadId,
                               });
-      // LOG(INFO) << "Node #" << threadId << " sent a msg to node #"
-                // << destThreadId << " at round #" << r;
       break;
     }
     if (threadId + r < n) {
-      // LOG(INFO) << "Node #" << threadId << " is waiting for a msg";
       auto msg = mq.receive(threadId);
-      // LOG(INFO) << "Node #" << threadId << " received a msg from node #"
-                // << msg.from;
       value += msg.v;
     }
   }
@@ -176,6 +170,63 @@ int simulateBinaryTreeReduce(std::vector<int>& values) {
       values[i] += values[i + r];
       // LOG(INFO) << "Node #" << (i + r) << " sent a msg to node #" << i;
     }
+  }
+  return values[0];
+}
+
+void ringAllReduceThreadRun(size_t threadId, std::vector<int> &values,
+                            MessageQueue<Message> &mq, Fence& fence) {
+  const size_t n = values.size();
+  size_t prevId = (threadId + n - 1) % n;
+  size_t nextId = (threadId + 1) % n;
+  // scatter-reduce phase
+  for (size_t r = 0; r + 1 < n; ++r) {
+    const size_t chunkIdToSend = (threadId + n - r) % n;
+    const size_t chunkIdToRecv = (chunkIdToSend + n - 1) % n;
+    mq.sendTo(nextId, Message{
+                          .v = values[chunkIdToSend],
+                          .from = chunkIdToSend,
+                      });
+    // LOG(INFO) << "Node #" << threadId << " sent value @" << chunkIdToSend << " " << values[chunkIdToSend]
+              // << " to node #" << nextId;
+    auto msg = mq.receive(threadId);
+    values[chunkIdToRecv] += msg.v;
+    CHECK_EQ(chunkIdToRecv, msg.from);
+    // LOG(INFO) << "Node #" << threadId << " received value @" << chunkIdToRecv << " " << msg.v;
+    fence.waitFor(r, n);
+  }
+
+  // reduce sharing phase
+  for (size_t r = 0; r + 1 < n; ++r) {
+    const size_t chunkIdToSend = (threadId + 1 + n - r) % n;
+    const size_t chunkIdToRecv = (chunkIdToSend + n - 1) % n;
+    mq.sendTo(nextId, Message{
+                          .v = values[chunkIdToSend],
+                          .from = chunkIdToSend,
+                      });
+    auto msg = mq.receive(threadId);
+    values[chunkIdToRecv] = msg.v;
+    CHECK_EQ(chunkIdToRecv, msg.from);
+    fence.waitFor(r + n, n);
+  }
+}
+
+std::vector<int> ringAllReduce(std::vector<std::vector<int>>& values) {
+  const auto n = values.size();
+  MessageQueue<Message> mq;
+  LOG(INFO) << "----------- Running " << n << " theads -----------";
+  std::vector<std::thread> threads;
+  Fence fence;
+  for (size_t i = 0; i < n; ++i) {
+    threads.emplace_back([&, i]() { 
+      ringAllReduceThreadRun(i, values[i], mq, fence);
+    });
+  }
+  for (auto& thread : threads) {
+    thread.join();
+  }
+  for (const auto &v : values) {
+    EXPECT_EQ(v, values[0]);
   }
   return values[0];
 }
@@ -233,6 +284,28 @@ TEST(Reduce, bianryTreeReduceWithMsgTest) {
   SCOPED_TRACE("test 1"); testN(1);
   SCOPED_TRACE("test 101"); testN(101);
   SCOPED_TRACE("test 1017"); testN(1017);
+}
+
+TEST(Reduce, ringAllReduceTest) {
+  auto testN = [](size_t n) {
+    std::vector<std::vector<int>> values(n);
+    int s = 0;
+    for (size_t i = 0; i < n; ++i) {
+      values[i] = std::vector<int>(n, i);
+      s += i;
+    }
+
+    const auto ans = std::vector<int>(n, s);
+
+    EXPECT_EQ(ringAllReduce(values), ans);
+  };
+
+  SCOPED_TRACE("test 1"); testN(1);
+  SCOPED_TRACE("test 2"); testN(2);
+  SCOPED_TRACE("test 9"); testN(9);
+  SCOPED_TRACE("test 1"); testN(1);
+  SCOPED_TRACE("test 101"); testN(101);
+  SCOPED_TRACE("test 102"); testN(102);
 }
 
 int main(int argc, char* argv[]) {
