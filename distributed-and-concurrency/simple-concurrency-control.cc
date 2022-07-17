aa8eda5b1dde84febc340cfeff612d49a6e7f355
HC Zhu <hczhu.mtv@gmail.com>
2022-07-17 15:11:29 -0700

    update

--- distributed-and-concurrency/simple-concurrency-control.cc
+++ distributed-and-concurrency/simple-concurrency-control.cc
@@ -56,6 +56,8 @@ class FooTest : public testing::Test {
   void TearDown() override {}
 };
 
+// https://en.cppreference.com/w/cpp/thread
+
 #include <latch>
 #include <functional>
 
@@ -63,50 +65,87 @@ using namespace std;
 
 class Foo {
 public:
-  Foo() : l1(1), l2(1) {}
+  Foo(bool use_atomic = false) : use_atomic_(use_atomic), l1(1), l2(1) {
+    LOG(INFO) << "Initial value: " << st;
+  }
+
+  void atomicTransit(int expected, int desired) {
+    const auto exp = expected;
+    while (!std::atomic_compare_exchange_strong(&st, &expected, desired)) {
+      expected = exp;
+    }
+  }
 
   void first(function<void()> printFirst) {
     // printFirst() outputs "first". Do not change or remove this line.
     printFirst();
-    l1.count_down();
+    if (use_atomic_) {
+      atomicTransit(0, 1);
+    } else {
+      l1.count_down();
+    }
   }
 
   void second(function<void()> printSecond) {
-    l1.wait();
+    if (use_atomic_) {
+      atomicTransit(1, 2);
+    } else {
+      l1.wait();
+    }
     // printSecond() outputs "second". Do not change or remove this line.
     printSecond();
-    l2.count_down();
+    if (use_atomic_) {
+      atomicTransit(2, 3);
+    } else {
+      l2.count_down();
+    }
   }
 
   void third(function<void()> printThird) {
-    l2.wait();
+    if (use_atomic_) {
+      atomicTransit(3, 4);
+    } else {
+      l2.wait();
+    }
     // printThird() outputs "third". Do not change or remove this line.
     printThird();
   }
+  bool use_atomic_;
   std::latch l1, l2;
+  std::atomic<int> st {0};
 };
 
 TEST_F(FooTest, Bar) {
-  std::mutex mt;
-  std::vector<int> output;
-  auto getFn = [&](int v) {
-    return [&, v] {
-      std::lock_guard l(mt);
-      LOG(INFO) << "Output " << v;
-      output.push_back(v);
+  auto oneTest = [](Foo &foo) {
+    std::mutex mt;
+    std::vector<int> output;
+    auto getFn = [&](int v) {
+      return [&, v] {
+        std::lock_guard l(mt);
+        LOG(INFO) << "Output " << v;
+        output.push_back(v);
+      };
     };
+
+    std::thread t3([&]() { foo.third(getFn(3)); });
+    std::thread t2([&]() { foo.second(getFn(2)); });
+    std::thread t1([&]() { foo.first(getFn(1)); });
+
+    t1.join();
+    t2.join();
+    t3.join();
+
+    EXPECT_EQ(std::vector<int>({1, 2, 3}), output);
   };
 
-  Foo foo;
-  std::thread t3([&]() { foo.third(getFn(3)); });
-  std::thread t2([&]() { foo.second(getFn(2)); });
-  std::thread t1([&]() { foo.first(getFn(1)); });
-
-  t1.join();
-  t2.join();
-  t3.join();
-
-  EXPECT_EQ(std::vector<int>({1, 2, 3}), output);
+  {
+    Foo foo;
+    oneTest(foo);
+  }
+  {
+    Foo foo(true);
+    oneTest(foo);
+  }
 }
 
 int main(int argc, char* argv[]) {
