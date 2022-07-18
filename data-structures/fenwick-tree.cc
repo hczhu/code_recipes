65eb0afd3c5c9ab0ab70670f656b4d063bd947eb
HC Zhu <hczhu.mtv@gmail.com>
2022-07-18 12:47:18 -0700

    update

---
+++ data-structures/fenwick-tree.cc
@@ -0,0 +1,274 @@
+
+#include <algorithm>
+#include <array>
+#include <atomic>
+#include <cassert>
+#include <cmath>
+#include <complex>
+#include <condition_variable>
+#include <cstdarg>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <fstream>
+#include <functional>
+#include <iostream>
+#include <iterator>
+#include <limits.h>
+#include <limits>
+#include <map>
+#include <memory>
+#include <mutex>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <string>
+#include <thread>
+#include <valarray>
+#include <vector>
+
+#include <gflags/gflags.h>
+#include <glog/logging.h>
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+template<typename T>
+class _DisplayType;
+
+template<typename T>
+void _displayType(T&& t);
+
+#define PEEK(x) LOG(INFO) << #x << ": [" << (x) << "]"
+
+#define _STR(x) #x
+#define STR(x) _STR(x)
+#define PRINT_MACRO(M) static_assert(0, "Macro " #M " = " STR(M))
+
+// #define x 42
+// PRINT_MACRO(x);
+
+/* template end */
+
+size_t ceilingTwo(size_t n) {
+  size_t res = 1;
+  while (res < n) {
+    res <<= 1;
+  }
+  return res;
+}
+
+// Least significant bit
+#define LSB(x) (((x) ^ ((x) - 1)) & (x))
+
+// Support cumulative operations, e.g. summation, on segments.
+template<typename Segment>
+class FenwickTree {
+ public:
+   using SegCallback = std::function<void(Segment &)>;
+
+   FenwickTree(size_t n) : n_(ceilingTwo(n)), points_(n_ + 1), segs_(n_ + 1) {}
+
+   size_t size() const { return n_; }
+
+   Segment &getPoint(size_t idx) & { return points_[idx]; }
+
+   // Visit all parent segments consisting of 'pointIdx' from the shortest one
+   // to the longest one.
+   // E.g. 18 (10010) will visit the following segments in the order
+   //    18 (10010) => (16, 18]
+   //    20 (10100) => (16, 20]
+   //    24 (11000) => (16, 24]
+   //    32 (100000) => (0, 32]
+   //    assuming n_ is 32
+   void traverseParentSegments(size_t pointIdx, SegCallback &&fn) {
+     if (pointIdx == 0) {
+       return;
+     }
+     for (size_t segIdx = pointIdx; segIdx <= n_; segIdx += LSB(segIdx)) {
+       fn(segs_[segIdx]);
+     }
+   }
+
+   // Visit all a disjoint set of segments which combined exactly cover [l, r)
+   // from the rightmost one to the leftmost one. E.g. [3, 8) or (2, 7] will be
+   // covered by segments and points
+   //     (6, 7]
+   //     (4, 6]
+   //     4  (point)
+   //     (2, 3]
+   //
+   // 'fn' can record cumulative operations on a segment covered by [l, r).
+   void traverseSegmentsFor(size_t l, size_t r, SegCallback &&fn) {
+     --l;
+     --r;
+     // To cover (l, r] now.
+     while (r > l) {
+       auto nr = r - LSB(r);
+       if (nr >= l) {
+         fn(segs_[r]);
+         r = nr;
+       } else {
+         fn(points_[r]);
+         --r;
+       }
+     }
+   }
+
+   void traverseChildSegments(size_t segIdx, SegCallback &&fn) {
+     fn(points_[segIdx]);
+     size_t l = segIdx - LSB(segIdx) + 1;
+     size_t r = segIdx;
+     traverseSegmentsFor(l, r, std::move(fn));
+   }
+
+   size_t grow() {
+     n_ <<= 1;
+     points_.resize(n_ + 1);
+     segs_.resize(n_ + 1);
+     return n_;
+   }
+
+ protected: 
+  // Support all indexes in [1, n_].
+  size_t n_;
+  // 1-based index. points_[0] is not used.
+  std::vector<Segment> points_;
+  // 1-based index. segs_[0] is not used.
+  // segs_[i] represents segment (i - LSB(i), i].
+  // E.g. segs_[8] => (0, 8]
+  //      segs_[7] => (6, 7]
+  //      segs_[6] => (4, 6]
+  std::vector<Segment> segs_;
+};
+
+
+// Supports Range Minimum Queries (RMQ)
+template<typename Seg, typename Comp>
+class RMQ {
+ public:
+  RMQ(size_t n) : tree_(n), comp() { }
+   
+
+ private:
+  FenwickTree<Seg> tree_;
+  Comp comp_;
+};
+
+
+class SegmentTreeTest : public testing::Test {
+ protected:
+  void SetUp() override {}
+  void TearDown() override {}
+};
+
+TEST_F(SegmentTreeTest, summation) {
+  FenwickTree<int> sumTree(3);
+  // A segment [l ,r) records the sum of all ops covering the whole segment.  
+  sumTree.getPoint(3) += 3;
+
+  sumTree.traverseSegmentsFor(1, 4, [](int &sum) { sum += 1; });
+
+  sumTree.grow();
+  auto expect = [&](size_t pidx) {
+    int res = sumTree.getPoint(pidx);
+    sumTree.traverseParentSegments(pidx,
+                                   [&res](int &sum) { res += sum; });
+    return res;
+  };
+  EXPECT_EQ(1, expect(1));
+  EXPECT_EQ(1, expect(2));
+  EXPECT_EQ(4, expect(3));
+
+  sumTree.traverseSegmentsFor(3, 7, [](int &sum) { sum -= 2; });
+
+  EXPECT_EQ(1, expect(2));
+  EXPECT_EQ(2, expect(3));
+  EXPECT_EQ(-2, expect(4));
+  EXPECT_EQ(-2, expect(5));
+  EXPECT_EQ(-2, expect(6));
+  EXPECT_EQ(0, expect(7));
+  EXPECT_EQ(0, expect(8));
+}
+
+struct Seg {
+  // a * x + b
+  size_t x = 0;
+  size_t a = 1;
+  size_t b = 0;
+};
+
+class Fancy {
+  static constexpr size_t M = 1e9 + 7;
+
+public:
+  Fancy() : tree_(2) {}
+
+  void append(int val) {
+    ++n_;
+    while (n_ > tree_.size()) {
+      tree_.grow();
+    }
+    tree_.getPoint(n_).x = size_t(val);
+  }
+
+  void addAll(int inc) {
+    tree_.traverseSegmentsFor(1, n_ + 1,
+                              [inc](Seg &seg) { seg.b = (seg.b + inc) % M; });
+  }
+
+  void multAll(int m) {
+    tree_.traverseSegmentsFor(1, n_ + 1, [m](Seg &seg) {
+      seg.a = (seg.a * m) % M;
+      seg.b = (seg.b * m) % M;
+    });
+  }
+
+  int getIndex(int idx) {
+    ++idx;
+    if (idx > n_) {
+      return -1;
+    }
+    auto val = tree_.getPoint(idx);
+    size_t res = ((val.a * val.x) + val.b) % M;
+    std::cout << val.a << " * " << val.x << " + " << val.b << std::endl;
+    tree_.traverseParentSegments(idx, [&res](Seg &seg) {
+      std::cout << seg.a << " * " << res << " + " << seg.b << std::endl;
+      res = ((res * seg.a) + seg.b) % M;
+    });
+
+    std::cout << "==========" << std::endl;
+    return res;
+  }
+
+private:
+  FenwickTree<Seg> tree_;
+  size_t n_ = 0;
+};
+
+TEST_F(SegmentTreeTest, LeetCodeFancy) {
+  Fancy fancy;
+
+  fancy.append(2);   // fancy sequence: [2]
+  fancy.addAll(3);   // fancy sequence: [2+3] -> [5]
+  EXPECT_EQ(5, fancy.getIndex(0)); // return 10
+  fancy.append(7);   // fancy sequence: [5, 7]
+  fancy.multAll(2);  // fancy sequence: [5*2, 7*2] -> [10, 14]
+  EXPECT_EQ(10, fancy.getIndex(0)); // return 10
+
+  fancy.addAll(3);   // fancy sequence: [10+3, 14+3] -> [13, 17]
+  fancy.append(10);  // fancy sequence: [13, 17, 10]
+  fancy.multAll(2);  // fancy sequence: [13*2, 17*2, 10*2] -> [26, 34, 20]
+
+  EXPECT_EQ(26, fancy.getIndex(0)); // return 26
+  EXPECT_EQ(34, fancy.getIndex(1)); // return 34
+  EXPECT_EQ(20, fancy.getIndex(2)); // return 20
+}
+
+int main(int argc, char* argv[]) {
+  testing::InitGoogleTest(&argc, argv);
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+  google::InitGoogleLogging(argv[0]);
+  return RUN_ALL_TESTS();
+}
+
---
+++ data-structures/lru_cache.cpp
@@ -0,0 +1,436 @@
+#include <algorithm>
+#include <array>
+#include <atomic>
+#include <cassert>
+#include <cmath>
+#include <complex>
+#include <condition_variable>
+#include <cstdarg>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <fstream>
+#include <functional>
+#include <iostream>
+#include <iterator>
+#include <limits.h>
+#include <limits>
+#include <map>
+#include <memory>
+#include <mutex>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <string>
+#include <thread>
+#include <unordered_set>
+#include <valarray>
+#include <vector>
+
+#include <gflags/gflags.h>
+#include <glog/logging.h>
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+template <typename T>
+class _DisplayType;
+
+template <typename T>
+void _displayType(T&& t);
+
+#define PEEK(x) LOG(INFO) << #x << ": [" << (x) << "]"
+
+/* template end */
+
+template <typename K, typename V>
+class LruCache {
+ public:
+  explicit LruCache(size_t maxItems) : maxItems_(maxItems) {}
+  virtual ~LruCache() = default;
+
+  virtual void set(const K& k, const V& v) = 0;
+  virtual const V* get(const K& k) = 0;
+
+ protected:
+  const size_t maxItems_;
+};
+
+template <typename K,
+          typename V,
+          template <typename, typename, typename>
+          typename HashSet>
+class LruCacheWithPointers : public LruCache<K, V> {
+ public:
+  using Super = LruCache<K, V>;
+
+  explicit LruCacheWithPointers(size_t maxItems) : LruCache<K, V>(maxItems) {
+    head_.next = &tail_;
+    tail_.prev = &head_;
+  }
+
+  void set(const K& k, const V& v) override {
+    if (auto ptr = getImp(k)) {
+      ptr->v = v;
+      return;
+    }
+
+    if (nodes_.size() == Super::maxItems_) {
+      const auto deleted = nodes_.erase(*popTail());
+      CHECK_EQ(1, deleted);
+    }
+
+    const auto pr = nodes_.insert(Node{
+        .k = k,
+        .v = v,
+    });
+    CHECK(pr.second);
+    pushToHead(&*(pr.first));
+  }
+
+  const V* get(const K& k) override {
+    auto ptr = getImp(k);
+    return ptr ? &ptr->v : nullptr;
+  }
+
+ private:
+  struct Node {
+    K k;
+    mutable V v;
+    mutable const Node* prev = nullptr;
+    mutable const Node* next = nullptr;
+  };
+  struct Hasher {
+    size_t operator()(const Node& node) const noexcept {
+      return hasher(node.k);
+    }
+
+    std::hash<K> hasher;
+  };
+  struct Equal {
+    bool operator()(const Node& l, const Node& r) const { return l.k == r.k; }
+  };
+
+  using Set = HashSet<Node, Hasher, Equal>;
+
+  const Node* getImp(const K& k) {
+    Node node{
+        .k = k,
+    };
+    auto itr = nodes_.find(node);
+    if (itr == nodes_.end()) {
+      return nullptr;
+    }
+    auto nodePtr = &(*itr);
+    unlinkNode(nodePtr);
+    pushToHead(nodePtr);
+    return nodePtr;
+  }
+  void unlinkNode(const Node* nodePtr) {
+    nodePtr->prev->next = nodePtr->next;
+    nodePtr->next->prev = nodePtr->prev;
+  }
+
+  void pushToHead(const Node* nodePtr) {
+    nodePtr->next = head_.next;
+    nodePtr->next->prev = nodePtr;
+    nodePtr->prev = &head_;
+    head_.next = nodePtr;
+  }
+
+  const Node* popTail() {
+    auto nodePtr = tail_.prev;
+    nodePtr->prev->next = &tail_;
+    tail_.prev = nodePtr->prev;
+    return nodePtr;
+  }
+
+  Node head_;
+  Node tail_;
+  Set nodes_;
+};
+
+template <typename K,
+          typename V,
+          template <typename, typename, typename, typename>
+          typename HashSet>
+class LruCacheWithIndexes : public LruCache<K, V> {
+ public:
+  using Super = LruCache<K, V>;
+  using Idx = uint32_t;
+
+  explicit LruCacheWithIndexes(size_t maxItems)
+      : LruCache<K, V>(maxItems),
+        nodes_(maxItems + 2),
+        tail_(maxItems + 1),
+        nodeSet_(maxItems, Allocator(nodes_)) {
+    nodes_[0].next = 1;
+    nodes_[tail_].prev = tail_ - 1;
+    for (Idx i = 1; i <= Super::maxItems_; ++i) {
+      nodes_[i].prev = i - 1;
+      nodes_[i].next = i + 1;
+    }
+  }
+
+  void set(const K& k, const V& v) override {
+    if (auto ptr = getImp(k)) {
+      ptr->v = v;
+      return;
+    }
+
+    if (nodes_.size() == Super::maxItems_) {
+      const auto deleted = nodeSet_.erase(nodes_[nodes_[0].next]);
+      CHECK_EQ(1, deleted);
+    }
+
+    Node node{
+        .k = k,
+        .v = v,
+    };
+    const auto pr = nodeSet_.insert(std::move(node));
+    CHECK(pr.second);
+
+    const Idx idx = nodes_[0].next;
+    CHECK_EQ(nodes_[idx].k, k);
+    CHECK_EQ(nodes_[idx].v, v);
+
+    nodes_[0].next = nodes_[idx].next;
+    nodes_[nodes_[0].next].prev = 0;
+
+    nodes_[idx].next = tail_;
+    nodes_[idx].prev = nodes_[tail_].prev;
+    nodes_[tail_].prev = idx;
+    nodes_[nodes_[idx].prev].next = idx;
+  }
+
+  const V* get(const K& k) override {
+    auto ptr = getImp(k);
+    return ptr ? &ptr->v : nullptr;
+  }
+
+ private:
+  struct Node {
+    K k;
+    mutable V v;
+    Idx prev;
+    Idx next;
+  };
+  std::vector<Node> nodes_;
+  const Idx tail_;
+
+  struct Hasher {
+    size_t operator()(const Node& node) const noexcept {
+      return hasher(node.k);
+    }
+    std::hash<K> hasher;
+  };
+  struct Equal {
+    bool operator()(const Node& l, const Node& r) const { return l.k == r.k; }
+  };
+  struct Allocator : std::allocator<Node> {
+    explicit Allocator(std::vector<Node>& nodesArg) : nodes(nodesArg) {
+      LOG(INFO) << "Constructed an Allocator @" << this;
+    }
+
+    Node* allocate(std::size_t n) {
+      CHECK_EQ(1, n) << "Can only allocate one node.";
+      Idx ret = nodes[0].next;
+      nodes[0].next = nodes[ret].next;
+      nodes[nodes[0].next].prev = 0;
+      LOG(INFO) << "Allocated node @" << ret;
+      return &nodes[ret];
+    }
+
+    void deallocate(Node* p, std::size_t n) {
+      CHECK_EQ(1, n) << "Can only deallocate one node.";
+      const auto idx = p - &nodes[0];
+      nodes[idx].next = nodes[0].next;
+      nodes[idx].prev = 0;
+      nodes[nodes[idx].next].prev = idx;
+      nodes[0].next = idx;
+      LOG(INFO) << "Deallocated node @" << idx;
+    }
+
+    template <typename U>
+    using other = std::allocator<U>;
+
+    std::vector<Node>& nodes;
+  };
+
+  using Set = HashSet<Node, Hasher, Equal, Allocator>;
+  Set nodeSet_;
+
+  const Node* getImp(const K& k) {
+    Node node{
+        .k = k,
+    };
+    auto itr = nodeSet_.find(node);
+    if (itr == nodeSet_.end()) {
+      return nullptr;
+    }
+    const auto idx = &(*itr) - &nodes_[0];
+
+    nodes_[nodes_[idx].prev].next = nodes_[idx].next;
+    nodes_[nodes_[idx].next].prev = nodes_[idx].prev;
+
+    nodes_[idx].next = tail_;
+    nodes_[idx].prev = nodes_[tail_].prev;
+    nodes_[nodes_[idx].prev].next = idx;
+    nodes_[nodes_[idx].next].prev = idx;
+    return &nodes_[idx];
+  }
+};
+
+class LruCacheTest : public testing::Test {
+ protected:
+  void SetUp() override {}
+  void TearDown() override {}
+
+  template <template <typename, typename> typename LruCache>
+  void doTest() {
+    LruCache<int, int> cache(3);
+    EXPECT_EQ(nullptr, cache.get(0));
+    cache.set(0, 0);
+    EXPECT_EQ(0, *cache.get(0));
+    cache.set(0, 1);
+    EXPECT_EQ(1, *cache.get(0));
+
+    cache.set(1, 1);
+    cache.set(2, 2);
+    EXPECT_EQ(1, *cache.get(0));
+    EXPECT_EQ(2, *cache.get(2));
+    EXPECT_EQ(1, *cache.get(1));
+
+    cache.set(3, 3);
+    EXPECT_EQ(nullptr, cache.get(0));
+
+    cache.set(4, 4);
+    EXPECT_EQ(nullptr, cache.get(2));
+  }
+};
+
+template <typename K, typename V>
+using LruCacheWithPointersAndStdUnorderedSet =
+    LruCacheWithPointers<K, V, std::unordered_set>;
+
+template <typename K, typename V>
+using LruCacheWithIndexesAndStdUnorderedSet =
+    LruCacheWithIndexes<K, V, std::unordered_set>;
+
+
+#include <list>
+
+template <typename K, typename V>
+class LruCacheList {
+ public:
+  using Super = LruCache<K, V>;
+  
+  LruCacheList(size_t maxNumItems) : maxNumItems_(maxNumItems) {
+  }
+
+  void printList(const std::string& header) const {
+    std::cout << header;
+    for (auto itr = recencyList_.begin(); itr != recencyList_.end(); ++itr) {
+      std::cout << " " << *itr;
+    }
+    std::cout << std::endl;
+  }
+
+  void set(const K& k, const V& v) {
+    if (auto vPtr = get(k)) {
+      *vPtr = v;
+      return;
+    }
+    if (k2Node_.size() == maxNumItems_) {
+      const auto& keyToEvict = *recencyList_.rbegin();
+      LOG(INFO) << "Evicting " << keyToEvict;
+      const auto numErased = k2Node_.erase(keyToEvict);
+      CHECK_EQ(1, numErased);
+      recencyList_.pop_back();
+    }
+
+    recencyList_.insert(recencyList_.begin(), k);
+    Node node{
+      .v = v,
+      .itr = recencyList_.begin(),
+    };
+    k2Node_.emplace(k, node);
+    printList("set()");
+  }
+  
+  V* get(const K& k) {
+    auto itr = k2Node_.find(k);
+    if (itr == k2Node_.end()) {
+      return nullptr;
+    }
+    auto& node = itr->second;
+    recencyList_.splice(recencyList_.begin(), recencyList_, node.itr);
+    printList("get()");
+    return &node.v;
+  }
+
+ private:
+  const size_t maxNumItems_;
+  // .begin() is the most recently accessed key.
+  std::list<K> recencyList_;
+
+  struct Node {
+    V v;
+    // refer to the element in the 'recencyList_'
+    const typename decltype(recencyList_)::const_iterator itr; 
+  };
+
+  std::unordered_map<K, Node> k2Node_;
+
+
+};
+
+TEST(stlList, Simple) {
+  std::list<int> l = {
+    1, 2, 3, 4
+  };
+  auto it = l.begin();
+  l.splice(l.begin(), l, it);
+  EXPECT_EQ(1, *it);
+  ++it;
+  EXPECT_EQ(2, *it);
+
+  l.splice(l.begin(), l, it);
+  EXPECT_EQ(2, *it);
+  ++it;
+  EXPECT_EQ(1, *it);
+
+  {
+    std::list<int> l = {1,};
+    auto it = l.begin();
+    l.splice(l.begin(), l ,it);
+    EXPECT_EQ(1, *it);
+    ++it;
+    EXPECT_EQ(l.end(), it);
+  }
+}
+
+TEST_F(LruCacheTest, Simple) {
+  {
+    SCOPED_TRACE("LruCacheWithPointersAndStdUnorderedSet");
+    doTest<LruCacheWithPointersAndStdUnorderedSet>();
+  }
+  {
+    SCOPED_TRACE("LruCacheWithIndexesAndStdUnorderedSet");
+    // doTest<LruCacheWithIndexesAndStdUnorderedSet>();
+  }
+
+  {
+    SCOPED_TRACE("LruCacheList");
+    doTest<LruCacheList>();
+  }
+}
+
+
+
+int main(int argc, char* argv[]) {
+  testing::InitGoogleTest(&argc, argv);
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+  google::InitGoogleLogging(argv[0]);
+  return RUN_ALL_TESTS();
+}
