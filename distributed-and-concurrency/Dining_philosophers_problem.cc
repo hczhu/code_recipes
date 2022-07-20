3c873a1135e3d9f13dcab00ee318b4fd7fa2a4d1
HC Zhu <hczhu.mtv@gmail.com>
2022-07-19 17:39:42 -0700

    update

--- distributed-and-concurrency/Dining_philosophers_problem.cc
+++ distributed-and-concurrency/Dining_philosophers_problem.cc
@@ -53,21 +53,23 @@ void _displayType(T&& t);
 
 // https://www.wikiwand.com/en/Dining_philosophers_problem
 //
-#if __cplusplus >= 202002L
+#if __cplusplus >= 202002L and 0
 // C++20 (and later) code
 #include <semaphore>
 using BinarySemaphore = std::binary_semaphore;
 #else
 
-class BinaySemaphore {
+class BinarySemaphore {
  public:
-   BinarySemaphore(size_t v) v_(std::min(1, std::max(0, v))) {}
+   BinarySemaphore(size_t v = 0)
+       : v_(std::min<size_t>(1, std::max<size_t>(0, v))) {}
+   BinarySemaphore(const BinarySemaphore &) = default;
    void release() & {
     {
       std::lock_guard lg(m_);
       v_ = 1;
     }
-    cv_.notify_all();
+    cv_.notify_one();
    }
 
    //A blocking API
@@ -84,12 +86,11 @@ class BinaySemaphore {
   std::mutex m_;
   size_t v_;
   std::condition_variable cv_;
-
 };
 
 #endif
 
-TEST(FooTest, Bar) {
+TEST(BinarySemaphoreTest, Basic) {
   BinarySemaphore bs(0);
   std::atomic<int> v{1};
   std::thread adder([&] { bs.acquire(); ++v; });
@@ -99,6 +100,67 @@ TEST(FooTest, Bar) {
   adder.join();
 
   EXPECT_EQ(3, v);
+}
+
+struct PhilosophersSync {
+  enum class State {
+    THINKING = 0, // philosopher is thinking
+    HUNGRY = 1,   // philosopher is trying to get forks
+    EATING = 2,   // philosopher is eating
+  };
+
+  PhilosophersSync(size_t n)
+      : numPhils_(n), states_(n, State::THINKING) {
+    for (size_t i = 0; i < numPhils_; ++i) {
+      sems_.push_back(std::make_unique<BinarySemaphore>(0));
+    }
+  }
+
+  // The caller must be holding 'm_'.
+  // It's a non-blocking API.
+  void tryToEat(size_t pid) & {
+    if (states_[pid] == State::HUNGRY
+        && states_[(pid + numPhils_ - 1) % numPhils_] != State::EATING
+        && states_[(pid + 1) % numPhils_] != State::EATING) {
+      states_[pid] = State::EATING;
+      // The beauty of this algorithm such that another philosopher can release
+      // the semaphore for 'pid' and starvation can be avoided by design.
+      sems_[pid]->release();
+    }
+  }
+
+  std::unique_lock<std::mutex> acquireLock() & {
+    return std::unique_lock<std::mutex>(m_);
+  }
+
+  void takeForks(size_t pid, std::function<void()> cb) & {
+    {
+      std::lock_guard<std::mutex> lg(m_);
+      states_[pid] = State::HUNGRY;
+      tryToEat(pid);
+    }
+    // May be blocked here until both left and right philosophers yield by
+    // calling sems_[pid].release() in 'tryToEat(pid)' on behalf of 'pid'.
+    sems_[pid]->acquire();
+    cb();
+  }
+
+  void putForks(size_t pid) & {
+    std::lock_guard<std::mutex> lg(m_);
+    states_[pid] = State::THINKING;
+    tryToEat((pid + 1) % numPhils_);
+    tryToEat((pid + numPhils_ - 1) % numPhils_);
+  }
+
+ private:
+  const size_t numPhils_;
+  std::mutex m_;
+  std::vector<State> states_;
+  std::vector<std::unique_ptr<BinarySemaphore>> sems_;
+};
+
+TEST(DiningPhilosophersTest, Basic) {
+
 }
 
 int main(int argc, char* argv[]) {
