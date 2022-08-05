d8239548bdbcd8d1707eb382f95ccec9405f3708
HC Zhu <hczhu.mtv@gmail.com>
2022-08-04 23:53:30 -0700

    update

--- ibm-ponder-this/2022_08.go
+++ ibm-ponder-this/2022_08.go
@@ -29,14 +29,14 @@ func CountDanceSequence(pn int) uint64 {
 		// f(2, 3) = 2 * f(0, 0) + 2 * f(1, 3)
 		{2, 0, 0, 2},
 		// f(3, 3) = 2 * f(0, 0)
-		{2, },
+		{2},
 	}
 	var pma [257][N][N]uint64
 	for i := 0; i < N; i++ {
 		for j := 0; j < N; j++ {
-	    pma[0][j][i] = ma[i][j]
-    }
-  }
+			pma[0][j][i] = ma[i][j]
+		}
+	}
 	ma_mul := func(a, b [N][N]uint64) [N][N]uint64 {
 		var res [N][N]uint64
 		for i := 0; i < N; i++ {
@@ -44,7 +44,7 @@ func CountDanceSequence(pn int) uint64 {
 				res[i][j] = 0
 				for k := 0; k < N; k++ {
 					res[i][j] += a[i][k] * b[k][j]
-				  res[i][j] %= M
+					res[i][j] %= M
 				}
 			}
 		}
@@ -58,29 +58,29 @@ func CountDanceSequence(pn int) uint64 {
 		ans[i][i] = 1
 	}
 	for i := 0; i < pn; i++ {
-    ans = ma_mul(ans, pma[i])
+		ans = ma_mul(ans, pma[i])
 	}
-  // fmt.Printf("%d\n", ans)
-  // n = 1 distribution {
-  //   4, 12,
-  // }
-  s := uint64(0)
-  for i := 0; i < N; i++ {
-    s += 4 * ans[i][0] + 12 * ans[i][1]
-  }
+	// fmt.Printf("%d\n", ans)
+	// n = 1 distribution {
+	//   4, 12,
+	// }
+	s := uint64(0)
+	for i := 0; i < N; i++ {
+		s += 4*ans[i][0] + 12*ans[i][1]
+	}
 	return s % M
 }
 
 func main() {
-  testOne := func(pn int, expect uint64) {
-	  if CountDanceSequence(pn) != expect {
-      fmt.Printf("f(%d) != %d\n", pn, expect)
-      panic("Error")
-    }
-  }
-  testOne(0, 16)
-  testOne(3, 17342172)
-  testOne(7, 2484449895)
+	testOne := func(pn int, expect uint64) {
+		if CountDanceSequence(pn) != expect {
+			fmt.Printf("f(%d) != %d\n", pn, expect)
+			panic("Error")
+		}
+	}
+	testOne(0, 16)
+	testOne(3, 17342172)
+	testOne(7, 2484449895)
 	fmt.Printf("%d\n", CountDanceSequence(24))
 	fmt.Printf("%d\n", CountDanceSequence(256))
 }
