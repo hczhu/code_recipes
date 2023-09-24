cec5a9f73dd3b22c993ee7a2340d36f07f60f421
HC Zhu <hczhu.mtv@gmail.com>
2023-09-23 18:32:10 -0700

    update

--- distributed-and-concurrency/async-spinner.py
+++ distributed-and-concurrency/async-spinner.py
@@ -1,24 +1,23 @@
 #!/usr/bin/env python3.10
 # -*- coding: utf-8 -*-
 
+import asyncio
 import itertools
 import time
 import logging
 import threading
 import multiprocessing as mp
-from typing import Any, TypeVar, Protocol, Type
+from typing import Any, TypeVar, Protocol, Type, Callable
 import typing
 
 class Event(Protocol):
-    e = threading.Event()
-    e.wait
     def wait(self, timeout: float | None) -> bool:
-        pass
+        return True
     def set(self) -> None:
         pass
 
 class Thread(Protocol):
-    def __init__(self) -> None:
+    def __init__(self, group: Any=None, target: Any=None, name: Any=None, args: Any=None) -> None:
         pass
     def start(self) -> None:
         pass
@@ -44,9 +43,10 @@ def spin(msg: str, done: TEvent):
             break
     blanks = ' ' * len(status)
     print(f"\r{blanks}\r", end="", flush=True)
+    logging.info("spin ended")
 
-def supervisor(event_type: typing.Type[TEvent], thread_type: typing.Type[TThread]):
-    done = event_type()
+def supervisor(event_factor: Callable[[], TEvent], thread_type: typing.Type[TThread]):
+    done = event_factor()
     spinner = thread_type(target=spin, args=("thinking!", done), name="spinner")
     logging.info(f"spinner object:{spinner}")
     spinner.start()
@@ -57,13 +57,44 @@ def supervisor(event_type: typing.Type[TEvent], thread_type: typing.Type[TThread
     spinner.join()
     return result
 
+async def coro_spin(msg: str):
+    """Prints a message while waiting for another thread to finish."""
+    logging.info("spin started")
+    status = ""
+    for char in itertools.cycle('|/-\\'):
+        status = f"\r{char} {msg}"
+        print(status, end="", flush=True)
+        try:
+            await asyncio.sleep(.1)
+        except asyncio.CancelledError:
+            break
+    blanks = ' ' * len(status)
+    print(f"\r{blanks}\r", end="", flush=True)
+    logging.info("spin ended")
+
+async def coro_slow() -> int:
+    """A slow function."""
+    await asyncio.sleep(3)
+    return 42
+
+async def coro_supervisor() -> int:
+    spinner = asyncio.create_task(coro_spin("thinking!"))
+    logging.info(f"spinner object:{spinner}")
+    result = await coro_slow()
+    spinner.cancel()
+    return result
+
 def main():
     logging.basicConfig(
         level=logging.INFO,
-        format="%(asctime)s p:%(processName)s t:%(threadName)s %(filename)s:%(lineno)s %(levelname)s: %(message)s"
+        format="p:%(processName)s t:%(threadName)s %(filename)s:%(lineno)s %(levelname)s: %(message)s"
     )
-    print(f"Answer: {supervisor(threading.Event, threading.Thread)}")
-    print(f"Answer: {supervisor(mp.Event, mp.Process)}")
+    start = time.perf_counter()
+    print(f"Answer: {supervisor(threading.Event, threading.Thread)} after {time.perf_counter() - start:.3f} seconds")
+    start = time.perf_counter()
+    print(f"Answer: {supervisor(mp.Event, mp.Process)} after {time.perf_counter() - start:.3f} seconds")
+    start = time.perf_counter()
+    print(f"Answer: {asyncio.run(coro_supervisor())} after {time.perf_counter() - start:.3f} seconds")
 
 if __name__ == "__main__":
     main()
