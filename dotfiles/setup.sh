5e363629148da79bd325a67f56241612bd2141a8
HC Zhu <hczhu.mtv@gmail.com>
2023-12-17 15:18:58 -0800

    update

--- dotfiles/setup.sh
+++ dotfiles/setup.sh
@@ -17,6 +17,11 @@ if [ -r ${HOME}/.dotfiles ]; then
   rm -i ${HOME}/.dotfiles
 fi
 ln -s ${dir_name} ${HOME}/.dotfiles
+
+if [[ -w ${HOME}/.bashrc ]]; then
+  echo ". ${dir_name}/${dotfile}" >> ${HOME}/.bashrc
+fi
+
 for dotfile in clang-format gitignore ctags bashrc gitconfig inputrc template.cpp tmux.conf vimrc hgrc; do
   if [ -r ${HOME}/.${dotfile} ]; then
     echo "${HOME}/.${dotfile} already exists! Skipped it."
