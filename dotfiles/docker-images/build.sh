44be06c5ee70d6d97796486d413405a897b00602
HC Zhu <hczhu.mtv@gmail.com>
2023-12-18 11:33:07 -0800

    update

---
+++ dotfiles/docker-images/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+prog_name=$(basename "$0")
+dir_name=$(cd "$(dirname "$0")" || exit 1; pwd -P)
+
+cd ${dir_name}/..
+DOCKER_BUILDKIT=1 docker build -t hczhu.dev -f docker-images/dev.Dockerfile .
