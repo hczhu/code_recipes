846325f005f32bfb6b1a6161cb7b19c35f7914d7
HC Zhu <hczhu.mtv@gmail.com>
2025-09-30 12:35:58 -0700

    update

---
+++ python/cross-double-links.py
@@ -0,0 +1,125 @@
+from typing import Generic, TypeVar, Self, Tuple
+from doubly_linked_list import DoubleLinks
+
+T = TypeVar("T")
+
+
+class MinMaxCounters(Generic[T]):
+    class Data:
+        def __init__(self, t: T | None = None, head: Self | None = None, cnt: int = 0, l: Self | None = None):
+            self.t = t
+            self.head = head
+            self.cnt = cnt
+            self.l = l
+
+    Dnode = DoubleLinks[Data]
+
+    def __init__(self):
+        self.max_head = MinMaxCounters.Dnode(t=self.Data())
+        self.min_tail = MinMaxCounters.Dnode(t=self.Data())
+        self.max_head.next = self.min_tail
+        self.min_tail.prev = self.max_head
+
+        self.index: dict[T, MinMaxCounters.Dnode] = {}
+
+    def remove(self, p: Dnode) -> None:
+        p.remove()
+        phead = p.get_data().head
+        if phead.get_data().l == p:
+            phead.get_data().l = p.next
+        if phead.get_data().l is None and phead != self.max_head and phead != self.min_tail:
+            phead.remove()
+
+    def add(self, p: Dnode, phead: Dnode) -> None:
+        p.get_data().head = phead
+        if phead.get_data().l is None:
+            phead.get_data().l = p
+        else:
+            phead.get_data().l.insert(p)
+
+    def inc(self, t: T) -> None:
+        pt = self.index.get(t, None)
+        if pt is None:
+            pt = MinMaxCounters.Dnode(
+                MinMaxCounters.Data(t=t, head=self.min_tail)
+            )
+            self.index[t] = pt
+
+        phead = pt.get_data().head
+        phead_prev = phead.prev
+        new_cnt = phead.get_data().cnt + 1
+        # NB: phead may have been deleted. Don't use.
+        self.remove(pt)
+        phead = None
+        if phead_prev.get_data().cnt != new_cnt:
+            phead_prev = phead_prev.insert(MinMaxCounters.Dnode(
+                self.Data(cnt=new_cnt)
+            ))
+        self.add(pt, phead_prev)
+
+    def dec(self, t: T) -> None:
+        pt = self.index.get(t, None)
+        if pt is None:
+            return
+        phead = pt.get_data().head
+        phead_next = phead.next
+        new_cnt = phead.get_data().cnt - 1
+        # print(f"Dec {t} from {phead.get_data().cnt} to {new_cnt}")
+        # NB: phead may have been deleted. Don't use.
+        self.remove(pt)
+        phead = None
+        if new_cnt == 0:
+            del self.index[t]
+            return
+        assert phead_next is not None and phead_next is not self.min_tail and phead_next is not self.max_head
+        if phead_next.get_data().cnt != new_cnt:
+            phead_next = phead_next.prev.insert(MinMaxCounters.Dnode(
+                self.Data(cnt=new_cnt)
+            ))
+        self.add(pt, phead_next)
+
+    def get_max(self) -> Tuple[T | None, int]:
+        dnode = self.max_head.next
+        if dnode.get_data().l is None:
+            return None, 0
+        return dnode.get_data().l.get_data().t, dnode.get_data().cnt
+
+    def get_min(self) -> Tuple[T | None, int]:
+        dnode = self.min_tail.prev
+        if dnode.get_data().l is None:
+            return None, 0
+        return dnode.get_data().l.get_data().t, dnode.get_data().cnt
+
+
+if __name__ == "__main__":
+    mmc = MinMaxCounters[str]()
+    mmc.inc("a")
+    assert mmc.get_max() == ("a", 1)
+    assert mmc.get_min() == ("a", 1), mmc.get_min()
+    mmc.inc("b")
+    assert mmc.get_max() == ("a", 1), mmc.get_max()
+    assert mmc.get_min() == ("a", 1)
+    mmc.inc("b")
+    assert mmc.get_max() == ("b", 2), mmc.get_max()
+    assert mmc.get_min() == ("a", 1)
+    mmc.inc("b")
+    assert mmc.get_max() == ("b", 3), mmc.get_max()
+    assert mmc.get_min() == ("a", 1)
+    mmc.inc("b")
+    assert mmc.get_max() == ("b", 4), mmc.get_max()
+    assert mmc.get_min() == ("a", 1)
+    mmc.dec("b")
+    assert mmc.get_max() == ("b", 3), mmc.get_max()
+    assert mmc.get_min() == ("a", 1), mmc.get_min()
+    mmc.dec("b")
+    assert mmc.get_max() == ("b", 2), mmc.get_max()
+    assert mmc.get_min() == ("a", 1), mmc.get_min()
+    mmc.dec("b")
+    assert mmc.get_max() == ("a", 1), mmc.get_max()
+    assert mmc.get_min() == ("a", 1), mmc.get_min()
+    mmc.dec("a")
+    assert mmc.get_max() == ("b", 1), mmc.get_max()
+    assert mmc.get_min() == ("b", 1), mmc.get_min()
+    mmc.dec("b")
+    assert mmc.get_max() == (None, 0), mmc.get_max()
+    assert mmc.get_min() == (None, 0), mmc.get_min()
---
+++ python/doubly_linked_list.py
@@ -0,0 +1,66 @@
+from typing import TypeVar, Generic, Self
+
+T = TypeVar("T")
+
+
+class DoubleLinks(Generic[T]):
+    def __init__(self, t: T | None = None, prev: Self | None = None, next: Self | None = None):
+        self.prev = prev
+        self.next = next
+        self.t = t
+
+    def get_data(self) -> T:
+        return self.t
+
+    # Return the node preceding it. If it's None, return the next.
+    def remove(self) -> Self | None:
+        prev, next = self.prev, self.next
+        if prev is not None:
+            prev.next = next
+            if next is not None:
+                next.prev = prev
+            return prev
+        if next is not None:
+            next.prev = None
+        return next
+
+    # Insert after self. Return the new node.
+    def insert(self, dl: Self) -> Self:
+        next = self.next
+        self.next = dl
+        dl.prev = self
+        dl.next = next
+        if next is not None:
+            next.prev = dl
+        return dl
+
+    def __iter__(self) -> "DoubleLinksIter":
+        return DoubleLinksIter(self)
+
+
+class DoubleLinksIter(Generic[T]):
+    def __init__(self, dl: DoubleLinks[T] | None):
+        self.dl = dl
+
+    def __next__(self) -> T:
+        if self.dl is None:
+            raise StopIteration
+        res = self.dl.t
+        self.dl = self.dl.next
+        return res
+
+
+if __name__ == "__main__":
+    p0 = DoubleLinks[int](0)
+    p1 = p0.insert(DoubleLinks(1))
+    assert list(p0) == [0, 1], list(p0)
+    p2 = p1.insert(DoubleLinks(2))
+    assert list(p0) == [0, 1, 2], list(p0)
+    p1.remove()
+    assert list(p0) == [0, 2], list(p0)
+    p3 = p0.insert(DoubleLinks(3))
+    assert list(p0) == [0, 3, 2], list(p0)
+    p0.remove()
+    assert list(p3) == [3, 2], list(p3)
+    p2.remove()
+    assert list(p3) == [3], list(p3)
